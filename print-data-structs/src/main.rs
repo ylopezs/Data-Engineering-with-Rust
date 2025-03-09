c580893081368517a3ba9415840fcc825858bf7d
Yonatan <granmocasin@gmail.com>
2025-03-09 05:03:58 +0000

    print example

---
+++ print-data-structs/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    println!("Common Rust Collections:");
+
+    // Sequences
+    println!("\n\tSequences:");
+    println!("\t\tVec: https://doc.rust-lang.org/std/vec/struct.Vec.html");
+    println!("\t\tVecDeque: https://doc.rust-lang.org/std/collections/struct.VecDeque.html");
+    println!("\t\tLinkedList: https://doc.rust-lang.org/std/collections/struct.LinkedList.html");
+
+    // Maps
+    println!("\n\tMaps:");
+    println!("\t\tHashMap: https://doc.rust-lang.org/std/collections/struct.HashMap.html");
+    println!("\t\tBTreeMap: https://doc.rust-lang.org/std/collections/struct.BTreeMap.html");
+
+    // Sets
+    println!("\n\tSets:");
+    println!("\t\tHashSet: https://doc.rust-lang.org/std/collections/struct.HashSet.html");
+    println!("\t\tBTreeSet: https://doc.rust-lang.org/std/collections/struct.BTreeSet.html");
+
+    // Misc
+    println!("\n\tMisc:");
+    println!("\t\tBinaryHeap: https://doc.rust-lang.org/std/collections/struct.BinaryHeap.html");
+}
