180837a9b9436a0dd4b6e89eaeaaabb888e97fb0
Yonatan <granmocasin@gmail.com>
2025-03-18 02:02:59 +0000

    vector example

---
+++ vector-fruit-salad/src/main.rs
@@ -0,0 +1,34 @@
+/*
+This program creates a fruit salad by scrambling (shuffling) a list of fruit.
+A vector is a growable array. It can grow or shrink in size and is one of the most
+useful data structures in Rust. A vector is represented using the Vec<T> type.
+*/
+
+use rand::seq::SliceRandom; // rand is a random number generation library in Rust
+use rand::thread_rng;
+
+fn main() {
+    let mut fruit = vec![
+        "Orange",
+        "Fig",
+        "Pomegranate",
+        "Cherry",
+        "Apple",
+        "Pear",
+        "Peach",
+    ];
+
+    // Scramble (shuffle) the fruit
+    let mut rng = thread_rng();
+    fruit.shuffle(&mut rng);
+
+    // Print out the fruit salad
+    println!("Fruit Salad:");
+    for (i, item) in fruit.iter().enumerate() {
+        if i != fruit.len() - 1 {
+            print!("{}, ", item);
+        } else {
+            println!("{}", item);
+        }
+    }
+}
