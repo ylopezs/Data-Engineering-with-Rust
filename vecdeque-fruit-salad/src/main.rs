9aa99a861d54cb87325e1370b7371658ee84e36f
Yonatan <granmocasin@gmail.com>
2025-03-18 02:22:36 +0000

    vecdeque exammple

---
+++ vecdeque-fruit-salad/src/main.rs
@@ -0,0 +1,53 @@
+/*
+This code starts with an initial VecDeque,
+converts it to a Vec for shuffling, and then converts it back to a VecDeque.
+After that, it pushes "Pomegranate" to the front of the deque, and "Fig" and "Cherry"
+to the back of the deque. Finally, it prints out the final fruit salad.
+
+A VecDeque is a double-ended queue, which means that you can push and pop from both ends
+of the queue.
+*/
+
+//TODO
+/*
+Challenge Questions:
+
+    Can you modify the program to allow the user to add fruits to either end of the queue after shuffling?
+
+    The SliceRandom trait provides a method choose(&self, rng: &R) -> Option<&T>. Can you use this to select a random fruit from the salad?
+
+    Can you adjust the program to remove a fruit from either end of the queue, displaying the name of the removed fruit and the state of the queue afterwards?
+*/
+use rand::seq::SliceRandom; // rand is a random number generation library in Rust
+use rand::thread_rng;
+use std::collections::VecDeque;
+
+fn main() {
+    let mut fruit: VecDeque<&str> = VecDeque::new();
+    fruit.push_back("Arbutus");
+    fruit.push_back("Loquat");
+    fruit.push_back("Strawberry Tree Berry");
+
+    // Scramble (shuffle) the fruit
+    let mut rng = thread_rng();
+    let mut fruit: Vec<_> = fruit.into_iter().collect();
+    fruit.shuffle(&mut rng);
+
+    // Convert it back to VecDeque
+    let mut fruit: VecDeque<_> = fruit.into_iter().collect();
+
+    // Add fruits to the both ends of the queue after shuffling
+    fruit.push_front("Pomegranate");
+    fruit.push_back("Fig");
+    fruit.push_back("Cherry");
+
+    // Print out the fruit salad
+    println!("Fruit Salad:");
+    for (i, item) in fruit.iter().enumerate() {
+        if i != fruit.len() - 1 {
+            print!("{}, ", item);
+        } else {
+            println!("{}", item);
+        }
+    }
+}
